d5bfaa02a71544441249eae160527fdc8a232179
Yudit <Yuditia@ad.royalprogress.com>
2025-12-10 10:23:33 +0700

    update

--- src/pages/api/auth/me.ts
+++ src/pages/api/auth/me.ts
@@ -5,7 +5,7 @@ import { prisma } from "@/lib/prisma";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const cookies = parseCookies(req);
-  const access = cookies["access_token"];
+  const access = cookies["accessToken"];
   if (!access) return res.status(401).json({ message: "No token" });
 
   try {
